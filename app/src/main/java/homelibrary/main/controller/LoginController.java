87bdef095ce143ed1961b54880b63b0afc22f1bb
Aasesi <mikolajwilczynnski@gmail.com>
2024-05-04 11:39:12 +0200

    feat: Implement login functionality
    Description: Implemented login feature allowing user to login. This includes Login controller and some small changes to login.html and start.html.

--- app/src/main/java/homelibrary/main/controller/LoginController.java
+++ app/src/main/java/homelibrary/main/controller/LoginController.java
@@ -1,7 +1,13 @@
 package homelibrary.main.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.*;
 
 /**
  *
@@ -11,10 +17,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController
 {
     @GetMapping("/login")
-    public String login()
-    {
-        /* Connect with the database to verify if the data is correct. */
-        // @Mikołaj
-        return "home" /* - if correct; "login" otherwise */;
+    public String loginGet() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@RequestParam("loginName") String username,
+                        @RequestParam("password") String password,
+                        HttpSession session,
+                        Model model) {
+        String dbUrl = "jdbc:postgresql://dsa-sec04.postgres.database.azure.com:5432/home_library";
+        String dbUsername = "dsa";;
+        String dbPassword = "b^&!78ieyvcKKzAce4tkNZi4qcrivKsA9@9W4KomsS$$B5yEmoCx#XpJmWSZn2msRxbo^B#toja*rWipEFDhquDp8rTvtbw7dxMie4%9gYqKTzr%&ht$hB$RXSA4p*&&";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
+            String query = "SELECT * FROM app.users WHERE username = ? AND password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, username);
+                statement.setString(2, password);
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    session.setAttribute("username", username);
+                    return "home";
+                } else {
+                    return "login";
+                }
+            }
+        } catch (SQLException e) {
+            return "login";
+        }
     }
 }
+
