608eb4bd3886a0460a5e4a2ac707fe35ab5a0156
Użytkownik <Uytkownik@KAMIL-KOMPUTER>
2024-05-10 22:52:42 +0200

    The page the AddServlet shall print is designed, together with the mechanism of adding the authors dynamically by means of a JavaScript script.

--- app/src/main/java/homelibrary/servlets/AddServlet.java
+++ app/src/main/java/homelibrary/servlets/AddServlet.java
@@ -34,7 +34,6 @@ public class AddServlet extends HttpServlet
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter())
         {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
