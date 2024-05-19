0d31a41378d725cfd400618f6d006ea39b800a70
JarnotMaciej <mj300741@student.polsl.pl>
2024-05-19 19:00:21 +0200

    editting stuff

--- app/src/main/java/homelibrary/servlets/Author.java
+++ app/src/main/java/homelibrary/servlets/Author.java
@@ -14,4 +14,12 @@ public class Author
         this.name = name;
         this.surname = surname;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
